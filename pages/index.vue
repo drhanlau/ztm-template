afd008792cab62eab2a95069aebfcf3497ae6dc4
Dr. Lau Cher Han <laucherhan@gmail.com>
2022-03-27 01:48:57 +0800

    test1

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Welcome to thelead.io!</h1>
-      <p class="mt-5">Turning Ideas Into Reality.</p>
+      <p class="mt-5">Turning Ideas Into Reality123.</p>
     </div>
   </div>
 </template>
